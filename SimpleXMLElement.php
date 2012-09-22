f6755ac8f4a07643e83722b3fb904df3037bb1b4
Sjors van der Pluijm <sjors@desjors.nl>
2012-09-22 11:06:36 +0200

    appendXML fails on PHP5.4

--- SimpleXMLElement.php
+++ SimpleXMLElement.php
@@ -37,18 +37,16 @@ class SimpleXMLElement extends \SimpleXMLElement {
 	 * @param SimpleXMLElement $append
 	 */
 	public function appendXML(SimpleXMLElement $append) {
-		if ($append) {
-			if (strlen(trim((string) $append)) == 0) {
-				$xml = $this->addChild($append->getName());
-				foreach ($append->children() as $child) {
-					$xml->appendXML($child);
-				}
-			} else {
-				$xml = $this->addChild($append->getName(), (string) $append);
+		if (strlen(trim((string) $append)) == 0) {
+			$xml = $this->addChild($append->getName());
+			foreach ($append->children() as $child) {
+				$xml->appendXML($child);
 			}
-			foreach ($append->attributes() as $n => $v) {
-				$xml->addAttribute($n, $v);
-			}
+		} else {
+			$xml = $this->addChild($append->getName(), (string) $append);
+		}
+		foreach ($append->attributes() as $n => $v) {
+			$xml->addAttribute($n, $v);
 		}
 	}
 
