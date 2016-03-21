d15ee0b3173abc862dd497ba4d1875d9c7c872b4
Roel van Duijnhoven <roel.duijnhoven@gmail.com>
2016-03-21 13:38:02 +0100

    Incorrectly assumed tabs were used.

--- LogTransportDecorator.php
+++ LogTransportDecorator.php
@@ -6,87 +6,87 @@ use Psr\Log\LoggerInterface;
 
 class LogTransportDecorator implements Transport
 {
-	/**
-	 * @var Transport
-	 */
-	protected $decorator;
+    /**
+     * @var Transport
+     */
+    protected $decorator;
 
-	/**
-	 * @var LoggerInterface
-	 */
-	protected $log;
+    /**
+     * @var LoggerInterface
+     */
+    protected $log;
 
-	/**
-	 * @param Transport $decorator
-	 * @param LoggerInterface $log
-	 */
-	public function __construct(Transport $decorator, LoggerInterface $log)
-	{
-		$this->decorator = $decorator;
-		$this->log = $log;
-	}
+    /**
+     * @param Transport $decorator
+     * @param LoggerInterface $log
+     */
+    public function __construct(Transport $decorator, LoggerInterface $log)
+    {
+        $this->decorator = $decorator;
+        $this->log = $log;
+    }
 
-	public function send($url, $requestMethod, $data = null, Array $headers = null)
-	{
-		$this->log->info(
-			'MoneyBird Request',
-			[
-				'url' => $url,
-				'requestMethod' => $requestMethod,
-				'data' => $data,
-				'headers' => $headers
-			]
-		);
+    public function send($url, $requestMethod, $data = null, Array $headers = null)
+    {
+        $this->log->info(
+            'MoneyBird Request',
+            [
+                'url' => $url,
+                'requestMethod' => $requestMethod,
+                'data' => $data,
+                'headers' => $headers
+            ]
+        );
 
-		try {
-			$response = $this->decorator->send($url, $requestMethod, $data, $headers);
+        try {
+            $response = $this->decorator->send($url, $requestMethod, $data, $headers);
 
-			$context = ['response' => $response];
+            $context = ['response' => $response];
 
-			$context = array_merge(
-				$context,
-				$this->getRequestLogContext()
-			);
+            $context = array_merge(
+                $context,
+                $this->getRequestLogContext()
+            );
 
-			$this->log->info('MoneyBird Response', $context);
-		} catch (\Moneybird\HttpClient\Exception $e) {
-			$context = ['exception' => $e];
+            $this->log->info('MoneyBird Response', $context);
+        } catch (\Moneybird\HttpClient\Exception $e) {
+            $context = ['exception' => $e];
 
-			$context = array_merge(
-				$context,
-				$this->getRequestLogContext()
-			);
+            $context = array_merge(
+                $context,
+                $this->getRequestLogContext()
+            );
 
-			$this->log->error('Response failed', $context);
+            $this->log->error('Response failed', $context);
 
-			throw $e;
-		}
+            throw $e;
+        }
 
-		return $response;
-	}
+        return $response;
+    }
 
-	/**
-	 * @return array
-	 */
-	protected function getRequestLogContext()
-	{
-		return $this->decorator->requestsLeft() === null
-			? []
-			: ['requestsLeft' => $this->decorator->requestsLeft()];
-	}
+    /**
+     * @return array
+     */
+    protected function getRequestLogContext()
+    {
+        return $this->decorator->requestsLeft() === null
+            ? []
+            : ['requestsLeft' => $this->decorator->requestsLeft()];
+    }
 
-	public function requestsLeft()
-	{
-		return $this->decorator->requestsLeft();
-	}
+    public function requestsLeft()
+    {
+        return $this->decorator->requestsLeft();
+    }
 
-	public function getLastResponse()
-	{
-		return $this->decorator->getLastResponse();
-	}
+    public function getLastResponse()
+    {
+        return $this->decorator->getLastResponse();
+    }
 
-	public function setUserAgent($userAgent)
-	{
-		return $this->decorator->setUserAgent($userAgent);
-	}
+    public function setUserAgent($userAgent)
+    {
+        return $this->decorator->setUserAgent($userAgent);
+    }
 }
--- tests/LogTransportDecoratorTest.php
+++ tests/LogTransportDecoratorTest.php
@@ -7,106 +7,106 @@ require_once dirname(__FILE__) . '/../vendor/psr/log/Psr/Log/LoggerInterface.php
 
 class LogTransportDecoratorTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * @var LogTransportDecorator
-	 */
-	protected $decorator;
+    /**
+     * @var LogTransportDecorator
+     */
+    protected $decorator;
 
-	/**
-	 * @var Transport
-	 */
-	protected $decorated;
+    /**
+     * @var Transport
+     */
+    protected $decorated;
 
-	/**
-	 * @var \Psr\Log\LoggerInterface
-	 */
-	protected $log;
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $log;
 
-	public static function setUpBeforeClass()
-	{
-		include ('config.php');
-	}
+    public static function setUpBeforeClass()
+    {
+        include ('config.php');
+    }
 
-	public function setUp()
-	{
-		$this->decorated = $this->getMock('Moneybird\Transport');
-		$this->log = $this->getMock('Psr\Log\LoggerInterface');
-		$this->decorator = new LogTransportDecorator($this->decorated, $this->log);
-	}
+    public function setUp()
+    {
+        $this->decorated = $this->getMock('Moneybird\Transport');
+        $this->log = $this->getMock('Psr\Log\LoggerInterface');
+        $this->decorator = new LogTransportDecorator($this->decorated, $this->log);
+    }
 
-	public function testLogRequestAndResponse()
-	{
-		$url = 'http://test.nl';
-		$method = 'POST';
-		$params = ['foo' => 'bar'];
-		$headers = ['cache-control' => 'public'];
-		$expectedResponse	= 'content';
+    public function testLogRequestAndResponse()
+    {
+        $url = 'http://test.nl';
+        $method = 'POST';
+        $params = ['foo' => 'bar'];
+        $headers = ['cache-control' => 'public'];
+        $expectedResponse    = 'content';
 
-		$this->decorated->expects($this->any())
-			->method('requestsLeft')
-			->will($this->returnValue(3));
+        $this->decorated->expects($this->any())
+            ->method('requestsLeft')
+            ->will($this->returnValue(3));
 
-		$this->decorated->expects($this->once())
-			->method('send')
-			->with($url, $method, $params, $headers)
-			->will($this->returnValue($expectedResponse));
+        $this->decorated->expects($this->once())
+            ->method('send')
+            ->with($url, $method, $params, $headers)
+            ->will($this->returnValue($expectedResponse));
 
-		$this->log->expects($this->at(0))
-			->method('info')
-			->with('MoneyBird Request', [
-				'url' => $url,
-				'requestMethod' => $method,
-				'data' => $params,
-				'headers' => $headers
-			]);
+        $this->log->expects($this->at(0))
+            ->method('info')
+            ->with('MoneyBird Request', [
+                'url' => $url,
+                'requestMethod' => $method,
+                'data' => $params,
+                'headers' => $headers
+            ]);
 
-		$this->log->expects($this->at(1))
-			->method('info')
-			->with('MoneyBird Response', [
-				'response' => $expectedResponse,
-				'requestsLeft' => 3
-			]);
+        $this->log->expects($this->at(1))
+            ->method('info')
+            ->with('MoneyBird Response', [
+                'response' => $expectedResponse,
+                'requestsLeft' => 3
+            ]);
 
-		$response = $this->decorator->send($url, $method, $params, $headers);
+        $response = $this->decorator->send($url, $method, $params, $headers);
 
-		$this->assertEquals($expectedResponse, $response);
-	}
+        $this->assertEquals($expectedResponse, $response);
+    }
 
-	/**
-	 * @expectedException \Moneybird\HttpClient\Exception
-	 */
-	public function testLogFailedRequest()
-	{
-			$url = 'http://test.nl';
-			$method = 'POST';
-			$params = ['foo' => 'bar'];
-			$headers = ['cache-control' => 'public'];
-			$exception = new \Moneybird\HttpClient\Exception('test');
+    /**
+     * @expectedException \Moneybird\HttpClient\Exception
+     */
+    public function testLogFailedRequest()
+    {
+            $url = 'http://test.nl';
+            $method = 'POST';
+            $params = ['foo' => 'bar'];
+            $headers = ['cache-control' => 'public'];
+            $exception = new \Moneybird\HttpClient\Exception('test');
 
-			$this->decorated->expects($this->any())
-				->method('requestsLeft')
-				->will($this->returnValue(4));
+            $this->decorated->expects($this->any())
+                ->method('requestsLeft')
+                ->will($this->returnValue(4));
 
-			$this->decorated->expects($this->once())
-				->method('send')
-				->will($this->throwException($exception));
+            $this->decorated->expects($this->once())
+                ->method('send')
+                ->will($this->throwException($exception));
 
-			$this->log->expects($this->at(0))
-				->method('info')
-				->with('MoneyBird Request', [
-					'url' => $url,
-					'requestMethod' => $method,
-					'data' => $params,
-					'headers' => $headers
-				]);
+            $this->log->expects($this->at(0))
+                ->method('info')
+                ->with('MoneyBird Request', [
+                    'url' => $url,
+                    'requestMethod' => $method,
+                    'data' => $params,
+                    'headers' => $headers
+                ]);
 
-			$this->log->expects($this->at(1))
-				->method('error')
-				->with('Response failed', [
-					'exception' => $exception,
-					'requestsLeft' => 4
-				]);
+            $this->log->expects($this->at(1))
+                ->method('error')
+                ->with('Response failed', [
+                    'exception' => $exception,
+                    'requestsLeft' => 4
+                ]);
 
-			$this->decorator->send($url, $method, $params, $headers);
-	}
+            $this->decorator->send($url, $method, $params, $headers);
+    }
 }
